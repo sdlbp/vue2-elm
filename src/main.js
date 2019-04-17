0e52a735c71ca5a65189b875cd167beade65d4d1
liubaopeng <lbpapp@163.com>
2019-04-17 18:40:14 +0800

    router 初步

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import routes from './router/router'
 import store from './store/'
-import {routerMode} from './config/env'
+import router from './router'
 import './config/rem'
 import FastClick from 'fastclick'
 
@@ -11,23 +9,6 @@ if ('addEventListener' in document) {
         FastClick.attach(document.body);
     }, false);
 }
-
-Vue.use(VueRouter)
-const router = new VueRouter({
-	routes,
-	mode: routerMode,
-	strict: process.env.NODE_ENV !== 'production',
-	scrollBehavior (to, from, savedPosition) {
-	    if (savedPosition) {
-		    return savedPosition
-		} else {
-			if (from.meta.keepAlive) {
-				from.meta.savedPosition = document.body.scrollTop;
-			}
-		    return { x: 0, y: to.meta.savedPosition || 0 }
-		}
-	}
-})
 
 new Vue({
 	router,
---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import VueRouter from 'vue-router'
+import Vue from 'vue'
+import routes from './routes'
+import { routerMode } from "../config/env";
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes,
+  mode: routerMode,
+  strict: process.env.NODE_ENV !== 'production',
+  scrollBehavior (to, from, savePosition) {
+    if (savePosition) {
+      return savePosition
+    }else {
+      if (from.meta.keepAlive) { // TODO 这个分支的作用
+        from.meta.savePosition = document.body.scrollTop;
+      }
+      return {x: 0, y: to.meta.savePosition || 0}
+    }
+  }
+})
+
+export default router
