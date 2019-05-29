ee049d03d39b72d3223de7534625003f0e373b05
liubaopeng <lbpapp@163.com>
2019-05-29 14:40:05 +0800

    format 配置文件

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  printWidth: 80, // 不折行最大长度
+  tabWidth: 2, // 缩进空格数
+  useTabs: false, // 是否使用tab 缩进
+  semi: false, // 末尾是否使用分号
+  singleQuote: true, // 是否使用单引号
+  trailingComma: 'none', // 尾逗号
+  bracketSpacing: true, // 对象字面量声明前后花括号是否包含空格
+  jsxBracketSameLine: false,
+  arrowParens: 'always', // 箭头函数添加圆括号
+  proseWrap: 'never', // 软折行
+  htmlWhitespaceSensitivity: 'strict',
+  endOfLine: 'lf'
+}
