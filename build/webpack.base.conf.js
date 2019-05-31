2b6f8680487183240a4c5e697cf41c81773ee367
liubaopeng <lbpapp@163.com>
2019-05-31 10:54:06 +0800

    format

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -51,31 +51,31 @@ module.exports = {
     loaders: [
       ...(config.dev.useEslint ? [createLintingRule()] : []),
       {
-      test: /\.vue$/,
-      loader: 'vue'
-    }, {
-      test: /\.js$/,
-      loader: 'babel',
-      include: projectRoot,
-      exclude: /node_modules/
-    }, {
-      test: /\.json$/,
-      loader: 'json'
-    }, {
-      test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-      loader: 'url',
-      query: {
-        limit: 10000,
-        name: utils.assetsPath('img/[name].[ext]')
-      }
-    }, {
-      test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-      loader: 'url',
-      query: {
-        limit: 10000,
-        name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
-      }
-    }]
+        test: /\.vue$/,
+        loader: 'vue'
+      }, {
+        test: /\.js$/,
+        loader: 'babel',
+        include: projectRoot,
+        exclude: /node_modules/
+      }, {
+        test: /\.json$/,
+        loader: 'json'
+      }, {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url',
+        query: {
+          limit: 10000,
+          name: utils.assetsPath('img/[name].[ext]')
+        }
+      }, {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url',
+        query: {
+          limit: 10000,
+          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+        }
+      }]
   },
   vue: {
     loaders: utils.cssLoaders({
